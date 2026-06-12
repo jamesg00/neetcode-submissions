926dd0329ba963395f996f95019180f27c17788c
jamesg00 <159197184+jamesg00@users.noreply.github.com>
2026-06-12 13:03:24 -0700

    Add: course-schedule - submission-27

---
+++ Data Structures & Algorithms/course-schedule/submission-27.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = defaultdict(list)
+        pre = prerequisites
+
+        for v, e in prerequisites:
+            graph[v].append(e)
+
+        unvisited = 0
+        visiting = 1
+        visited = 2
+
+        states = [unvisited] * numCourses
+
+        def dfs(node):
+            state = states[node]
+
+            if state == visiting: return False
+            elif state == visited: return True
+
+            states[node] = visiting
+
+            for n in graph[node]:
+                if not dfs(n): return False
+
+            states[node] = visited
+            return True 
+
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+
+
+
+
+            
+
+
+
+
+
+
+
+        
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+
