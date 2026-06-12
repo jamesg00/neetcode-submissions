b26601fc05f328637f5d05f458baf7740aaa3046
jamesg00 <159197184+jamesg00@users.noreply.github.com>
2026-06-11 21:53:44 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        newstr = ''
+
+        for char in s:
+
+            if char.isalnum():
+                newstr += char.lower()
+            
+            
+        
+        return newstr == newstr[::-1] 
